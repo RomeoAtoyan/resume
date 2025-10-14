4b5e5456fc3e30578e3bdab5c06b2a2e78b14853
RomeoAtoyan <atoyanr@gmail.com>
2025-10-14 16:03:26 +0200

    feat: remove resume action

--- lib/actions/remove-resume.ts
+++ lib/actions/remove-resume.ts
@@ -1,5 +1,32 @@
-"use client";
+import { ROUTES } from "@/constants/routes";
+import React, { SetStateAction } from "react";
 
-export const removeResume = (id: string) => {
-  console.log(id);
+export const removeResume = async ({
+  resumeId,
+  close,
+  router,
+  setLoading,
+}: {
+  resumeId: string;
+  close: () => void;
+  router: ReturnType<typeof import("next/navigation").useRouter>;
+  setLoading: React.Dispatch<SetStateAction<boolean>>;
+}) => {
+  if (!resumeId) {
+    console.warn("No resume ID provided — skipping removal.");
+    return;
+  }
+
+  try {
+    setLoading(true);
+    await fetch(`/api/resumes/${resumeId}`, {
+      method: "DELETE",
+    });
+    router.push(ROUTES.DASHBOARD);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+    close();
+  }
 };
--- modals/remove-resume.tsx
+++ modals/remove-resume.tsx
@@ -1,11 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { removeResume } from "@/lib/actions/remove-resume";
+import { useCvDataStore } from "@/store/use-cv-data-store";
 import { useModalStore } from "@/store/use-modal-store";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const RemoveResume = () => {
   const { close } = useModalStore();
+  const { resumeId } = useCvDataStore();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState<boolean>(false);
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex items-start gap-3">
@@ -24,7 +33,19 @@ const RemoveResume = () => {
         <Button onClick={() => close()} variant="outline">
           Cancel
         </Button>
-        <Button onClick={() => removeResume("123")} variant="destructive">Yes, Delete</Button>
+        <Button
+          onClick={() =>
+            removeResume({
+              resumeId: resumeId!,
+              close,
+              router,
+              setLoading,
+            })
+          }
+          variant="destructive"
+        >
+          {loading ? <Spinner /> : "Yes, Delete"}
+        </Button>
       </div>
     </div>
   );

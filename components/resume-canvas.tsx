954f5db6cee42116f491c925b92383a76c50ac95
RomeoAtoyan <atoyanr@gmail.com>
2025-10-18 02:24:04 +0200

    feat: resume canvas animation

--- components/resume-canvas.tsx
+++ components/resume-canvas.tsx
@@ -3,35 +3,44 @@
 import Default from "@/resume-templates/default";
 import Modern from "@/resume-templates/modern";
 import { useCvDataStore } from "@/store/use-cv-data-store";
-import { Button } from "./ui/button";
-import { handleDownloadResumePDF } from "@/lib/actions/resume/download-pdf-resume";
-import { useDownloadStore } from "@/store/use-download-store";
-import { getResumeContent } from "@/lib/helpers/get-resume-content";
+import { AnimatePresence, motion } from "framer-motion";
 
 const ResumeCanvas = () => {
   const { template } = useCvDataStore();
-  const { setDownloading } = useDownloadStore();
 
   const renderTemplate = () => {
     switch (template) {
       case "default":
-        return <Default />;
+        return <Default key="default" />;
       case "modern":
-        return <Modern />;
+        return <Modern key="modern" />;
       default:
-        return <div>How even?</div>;
+        return <div key="unknown">How even?</div>;
     }
   };
 
   return (
-    <div className="w-full h-full bg-[radial-gradient(circle,_#e5e7eb_1px,_transparent_1px)] [background-size:20px_20px] overflow-auto  flex items-center justify-center">
+    <div className="w-full h-full bg-[radial-gradient(circle,_#e5e7eb_1px,_transparent_1px)] [background-size:20px_20px] overflow-auto flex items-center justify-center">
       <div className="w-full h-full overflow-auto flex flex-col items-center gap-10 py-10">
-        <div
-          style={{ width: "210mm", minHeight: "297mm" }}
-          className="bg-white border shadow-sm p-6"
-        >
-          {renderTemplate()}
-        </div>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={template}
+            initial={{ opacity: 0, scale: 0.5, y: 20 }}
+            animate={{ opacity: 1, scale: 1, y: 0 }}
+            exit={{ opacity: 0, scale: 0.98, y: -20 }}
+            transition={{
+              duration: 0.4,
+              ease: [0.22, 1, 0.36, 1],
+            }}
+            style={{
+              width: "210mm",
+              minHeight: "297mm",
+            }}
+            className="bg-white border shadow-sm p-6 origin-center"
+          >
+            {renderTemplate()}
+          </motion.div>
+        </AnimatePresence>
       </div>
     </div>
   );

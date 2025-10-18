54d2a797c47854a14336d548e2c03b4c5495d415
RomeoAtoyan <atoyanr@gmail.com>
2025-10-18 02:42:22 +0200

    feat: root route redirection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-const Page = () => {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-4xl lg:text-9xl font-bold text-center">Next boilerplate</h1>
-    </div>
-  );
+import { ROUTES } from "@/constants/routes";
+import { getUser } from "@/lib/auth/get-user";
+import { redirect } from "next/navigation";
+
+const Page = async () => {
+  const user = await getUser();
+
+  if (!user) {
+    redirect(ROUTES.AUTH);
+  }
+
+  if (user) {
+    redirect(ROUTES.DASHBOARD);
+  }
 };
 
 export default Page;

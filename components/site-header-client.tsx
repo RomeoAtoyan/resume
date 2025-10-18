631c49741fac36d5299a0511d460c40a0b33d232
RomeoAtoyan <atoyanr@gmail.com>
2025-10-18 12:55:26 +0200

    feat: siteheader max-w styling

--- components/site-header-client.tsx
+++ components/site-header-client.tsx
@@ -17,10 +17,10 @@ const SiteHeaderClient = ({ user }: { user: User | null }) => {
       className={clsx(
         "w-full border-b border-gray-200 overflow-hidden",
         !isInBuilder && "sticky top-0 z-50 bg-white",
-        hideHeader && "hidden"
+        hideHeader && "hidden",
       )}
     >
-      <div className="flex items-center justify-between px-6 py-3">
+      <div className={clsx("flex items-center justify-between px-6 py-3", !isInBuilder && "max-w-6xl mx-auto")}>
         <div className="flex items-center gap-8">
           <NavLogo />
         </div>
